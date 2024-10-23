6dc9a14f378722ea8928057672857d1683f4331c
z524071 Hippolyte ANDRE <z524071@shibaura-it.ac.jp>
2024-10-23 15:13:30 +0900

    Update ahw4-1.c

--- ahw4-1.c
+++ ahw4-1.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+    int x,y,z,d;
+    scanf("%d",&x);
+    scanf("%d",&y);
+    scanf("%d",&z);
+    d = (x+y+z)/3;
+    printf("Average is %d\n", d);
+    return 0;
+}
 
